50126293d91efadfc520052e7c1012da8de462d3
Kyle Adkins <kyle.m.adkins@gmail.com>
2021-08-07 20:03:02 -0500

    Make post header responsive

--- template-parts/content.php
+++ template-parts/content.php
@@ -22,9 +22,9 @@
 			while (have_rows('content')) :
 				the_row();
 				if (get_row_layout() == 'header') : ?>
-					<div class="flex">
-						<div class="w-60 min-vh-100 cover bg-center" style="<?php if (get_field('hero_image')) : ?>background-image: url('<?php the_field('hero_image') ?>'); <?php endif; ?>"></div>
-						<div class="w-40 flex justify-center items-center tc ph4">
+					<div class="flex-l">
+						<div class="w-100 w-60-l vh-50 vh-100-l cover bg-center" style="<?php if (get_field('hero_image')) : ?>background-image: url('<?php the_field('hero_image') ?>'); <?php endif; ?>"></div>
+						<div class="w-100 w-40-l flex justify-center items-center tc ph4">
 							<div>
 								<div class="f6 ttu mt0 mb5 tracked archivo"><?php echo date('F Y', strtotime(get_field('date'))); ?></div>
 								<h1 class="f1 archivo mt0 mb3 ttu"><?php the_title(); ?></h1>
