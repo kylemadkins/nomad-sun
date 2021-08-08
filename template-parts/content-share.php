9e42c620c58649985c2c82d04ac7e47ce05a4641
Kyle Adkins <kyle.m.adkins@gmail.com>
2021-08-08 18:51:59 -0500

    Create and style "Share" component

---
+++ template-parts/content-share.php
@@ -0,0 +1,17 @@
+<div class="w5 center pv5 pv6-l">
+  <h4 class="archivo f6 ttu tracked mb4 tc relative">
+    <div class="mb4 line"></div>
+    Share
+  </h4>
+  <div class="flex items-center justify-center">
+    <a href="https://facebook.com" class="db mh2 link">
+      <img src="<?php bloginfo('template_directory') ?>/images/facebook-black.svg" class="db">
+    </a>
+    <a href="https://twitter.com" class="db mh2 link">
+      <img src="<?php bloginfo('template_directory') ?>/images/twitter-black.svg" class="db">
+    </a>
+    <a href="https://instagram.com" class="db mh2 link">
+      <img src="<?php bloginfo('template_directory') ?>/images/insta-black.svg" class="db">
+    </a>
+  </div>
+</div>
--- template-parts/content.php
+++ template-parts/content.php
@@ -33,10 +33,8 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php get_template_part('template-parts/content-explore'); ?>
-
 	<footer class="entry-footer">
-		<?php // nomad_sun_entry_footer(); 
-		?>
+		<?php get_template_part('template-parts/content-share'); ?>
+		<?php get_template_part('template-parts/content-explore'); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
